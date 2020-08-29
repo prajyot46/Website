8d9afd5c0aabd942b2943a28af1e60bf983d1e5e
shashankholla <shashu1999@gmail.com>
2020-08-29 17:16:45 +0530

    Fixed timeline

---
+++ assets/js/timelinedata.js
@@ -0,0 +1,76 @@
+var timelineData = new Vue({
+	el: "#timelineContainer",
+	data: {
+		timelineobject: [
+			{
+				image: "assets/img/img/1.jpg",
+				title: "2006",
+				desc: "Our Humble Beginnings",
+				content:
+					"In 2006, a group of students from the Mechanical Department of RVCE came up with the idea of fuel efficient yet robust cars giving rise to TEAM CHIMERA",
+			},
+			{
+				image: "assets/img/img/2.jpg",
+				title: "2009",
+				desc: "Hybrid Auto Rickshaw Battle",
+				content:
+					"Our business plan was adjudged the best and the project overall finished second.",
+			},
+			{
+				image: "assets/img/img/3.jpg",
+				title: "2011",
+				content:
+					"The only Indian team to successfully participate in Challenge Bibendum organized by Michelin in Berlin.",
+				desc: "Challenge Bibendum",
+			},
+			{
+				image: "assets/img/img/4.jpg",
+				title: "2014",
+				content:
+					"First Asian team to participate. Stood 9th in the competition bringing back the Fan Favourite Award.",
+				desc: "FSAE Hybrid - USA",
+			},
+			{
+				image: "assets/img/img/5.jpg",
+				title: "2015",
+				desc: "FSAE Electric - Italy ",
+				content:
+					"First venture into pure Electric cars after spending years in making Hybrid cars which made use of gas along with electricity for propulsion.",
+			},
+			{
+				image: "assets/img/img/6.jpg",
+				title: "2016",
+				desc: "FSAE Electric - Japan",
+				content:
+					"With improved car E-VOLVE v2 we bagged ROOKIE AWARD in the Electric Vehicle category",
+			},
+			{
+				image: "assets/img/img/7.jpeg",
+				title: "2017",
+				desc: "FSAE Electric - Japan",
+				content:
+					"We were placed 6th in business presentation in the electric category with our new car Chronos",
+			},
+			{
+				image: "assets/img/img/8.jpg",
+				title: "2018",
+				desc: "FSEV Concept Challenge - India",
+				content:
+					"We were placed 6th in business presentation in the electric category with our new car Chronos",
+			},
+			{
+				image: "assets/img/img/2019.jpg",
+				title: "2019",
+				desc: "Formula Bharat",
+				content:
+					"Participated in Formula Bharat 2019 held in Coimbatore,India and bagged 1st place in in Business Plan Presentation.",
+			},
+			{
+				image: "assets/img/img/2020.jpg",
+				title: "2020",
+				desc: "Formula Bharat",
+				content: "",
+			},
+		],
+	},
+});
